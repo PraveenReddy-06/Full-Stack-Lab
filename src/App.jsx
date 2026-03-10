6c077be9ab08447ae4fbe6164f7aa8a50cedad3c
Praveen Reddy <2400032662@kluniversity.in>
2026-03-10 08:09:39 +0530

    skill-10

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import StudentManager from './components/StudentManager'
+
+function App() {
+  return (
+    <div>
+      <StudentManager />
+    </div>
+      
+  )
+}
+
+export default App
